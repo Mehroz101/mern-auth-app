7236b2d67b54a3569bbd89e0b4e5b08faae611ac
Mehroz farooq <mehrozfarooq127@gmail.com>
2025-05-26 19:59:07 +0500

    change the code with correct one

--- Backend/src/constants/verifcationCode.ts
+++
@@ -1,5 +0,0 @@
-const enum VerificationCodeTypes {
-    EmailVerification = 'email_verification',
-    PasswordReset = 'password_reset',
-}
-export default VerificationCodeTypes;
--- Backend/src/utils/appAssert.ts
+++ Backend/src/utils/appAssert.ts
@@ -2,18 +2,21 @@ import assert from "node:assert";
 import AppError from "./AppError";
 import { HttpStatusCode } from "../constants/http";
 import AppErrorCode from "../constants/appErrorCode";
-type appAsserts = (
+
+type AppAssert = (
   condition: any,
-  HttpStatusCode: HttpStatusCode,
+  httpStatusCode: HttpStatusCode,
   message: string,
   appErrorCode?: AppErrorCode
 ) => asserts condition;
-
-
-const appAsserts: appAsserts = (
+/**
+ * Asserts a condition and throws an AppError if the condition is falsy.
+ */
+const appAssert: AppAssert = (
   condition,
-  HttpStatusCode,
+  httpStatusCode,
   message,
   appErrorCode
-) => assert(condition, new AppError(message, HttpStatusCode, appErrorCode));
-export default appAsserts;
+) => assert(condition, new AppError(httpStatusCode, message, appErrorCode));
+
+export default appAssert;
